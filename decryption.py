709f816df4d8d16bd70637e603fe3cb58a02665c
gabri <gabriele.dalo@stud.unifi.it>
2023-11-30 10:19:17 +0100

    Working decryption

--- decryption.py
+++ decryption.py
@@ -1,9 +1,7 @@
 import json
 
 from pyhpke import AEADId, CipherSuite, KDFId, KEMId, KEMKeyInterface
-from pyhpke.keys.x25519_key import X25519Key
 from pyhpke.kem import KEM
-from pyhpke.kem_key import KEMKey
 
 if __name__ == "__main__":
     # read the json file
@@ -17,40 +15,22 @@ if __name__ == "__main__":
         AEADId(int(data["aead_id"]))
     )
 
-    ikm = data["ikmR"].encode()
-    #keypair = suite_r.kem.derive_key_pair(ikm)
-
-    #my_pk = keypair.public_key
-    #my_sk = keypair.private_key
-
-    # get sender public key
-    #sender_ikm = data["ikmE"].encode()
-    #sender_keypair = suite_r.kem.derive_key_pair(sender_ikm)
-
-    #sender_pk = sender_keypair.public_key
-
-    # get information
-    my_sk = data["skRm"].encode()
-    my_pk = data["pkRm"].encode()
-    sender_pk = data["pkEm"].encode()
-    enc = data["enc"].encode() # PROBLEMA: enc dovrebbe essere in public_bytes
+    # le stringhe nei test vectors contengono due caratteri esadecimali per ogni byte
+    # quindi stringhe di 64 caratteri ad es., sono in realtà 32 bytes una volta decodificate
+    my_sk = suite_r.kem.deserialize_private_key(bytes.fromhex(data["skRm"]))
+    my_pk = suite_r.kem.deserialize_public_key(bytes.fromhex(data["pkRm"]))
+    sender_pk = suite_r.kem.deserialize_public_key(bytes.fromhex(data["pkEm"]))
+    enc = bytes.fromhex(data["enc"])
     
-    keys = KEM(KEMId(int(data["kem_id"])))
-    keys_prova = keys.derive_key_pair(ikm)
-    #my_sk = keys.deserialize_private_key(my_sk)
-    #my_pk = keys.deserialize_public_key(my_pk)
-    my_sk = keys_prova.private_key
-    my_pk = keys_prova.public_key
-    
-    
-    psk = b""
-    psk_id = b""
-    info = data["info"].encode()
-    aad = data["aad"].encode()
-    ct = data["ct"]
+    psk = bytes.fromhex(data["psk"]) if "psk" in data else b""
+    psk_id = bytes.fromhex(data["psk_id"]) if "psk_id" in data else b""
+    info = bytes.fromhex(data["info"]) if "info" in data else b""
+    aad = bytes.fromhex(data["aad"]) if "aad" in data else b""    
+    ct = bytes.fromhex(data["ct"]) if "ct" in data else b""
 
     receiving = suite_r.create_recipient_context(enc, my_sk, info, None, psk, psk_id)
 
-    in_message = receiving.open(ct, aad).decode()
-
-    print(in_message)
+    plaintext = receiving.open(ct, aad).decode()
+    original_plain_text = bytes.fromhex(data["pt"]).decode()
+    print(plaintext)
+    print(plaintext == original_plain_text)
